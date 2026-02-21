2a6bed857d2e2f7533c6b710ab8f0136b7046f7c
anhtuan11swd <anhtuan11.swd@gmail.com>
2026-02-21 10:43:09 +0700

    feat(server): configure port from environment variables

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
+import dotenv from "dotenv";
 import express from "express";
+
+dotenv.config();
 
 const app = express();
 
 app.get("/", (_req, res) => res.send("Xin chào từ backend"));
 
-app.listen(1000, () => {
-  console.log("Máy chủ đã khởi động");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server đã khởi động tại cổng ${PORT}`);
 });
